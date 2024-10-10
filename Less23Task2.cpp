883ab0bb6c79e94a44864d595c01e21eeeb3ac26
Havingworld <84483386+Havingworld@users.noreply.github.com>
2024-10-10 13:45:19 +1000

    Add files via upload

---
+++ Less23Task2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+#define SPRING "vesna"
+#define SUMMER "leto"
+#define AUTUMN "osen"
+#define WINTER "zima"
+
+#define BOOL_1 1
+#define BOOL_0 0
+
+#if BOOL_1
+int main()
+{
+    std::cout << SPRING << std::endl;
+}
+#endif
+
+#if BOOL_0
+int main()
+{
+    std::cout << STR(SUMMER) << std::endl;
+}
+#endif
+
+#if BOOL_0
+int main()
+{
+    std::cout << STR(AUTUMN) << std::endl;
+}
+#endif
+
+#if BOOL_0
+int main()
+{
+    std::cout << STR(WINTER) << std::endl;
+}
+#endif
